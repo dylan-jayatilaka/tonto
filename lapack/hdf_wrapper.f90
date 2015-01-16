03b4053f0a9f41aa6647a50f59a454e9067653a6
Peter Spackman <20265845@student.uwa.edu.au>
2015-01-16 15:38:08 +0800

    added initial wrapper for hdf library, placed in directory with blas and lapack for now

---
+++ lapack/hdf_wrapper.f90
@@ -0,0 +1,57 @@
+module hdf_wrapper
+  use h5lt
+  use hdf5
+  use iso_c_binding
+
+  implicit none
+
+  interface create_hdf_file
+    module procedure create_hdf_file
+  end interface
+
+  interface create_dataset_vec
+    module procedure cd_real_4_1d
+  end interface
+
+  interface create_dataset_mat
+  end interface
+
+contains
+
+  ! create file procedure
+  subroutine init_library
+    integer :: hdferr
+    call H5open_f(hdferr)
+  end subroutine
+
+  subroutine create_hdf_file(filename, loc_id)
+    character(len=*), intent(in) :: filename
+    integer, intent(out) :: loc_id
+    integer(hid_t) :: hdf_id
+    integer :: hdferr
+
+    call h5fcreate_f(filename, H5F_ACC_TRUNC_F, hdf_id, hdferr)
+
+    loc_id = hdf_id
+
+  end subroutine
+
+
+  subroutine cd_real_4_1d(loc_id, dataset_name, rank, buffer)
+    integer, intent(in) :: loc_id, rank
+    character(len=*), intent(in) :: dataset_name
+    real(4), dimension(:), intent(in) :: buffer
+    integer :: hdferr
+    integer(hid_t) :: hdf_id
+    integer(hsize_t), dimension(1) :: dims
+
+    dims = (/ 1 /)
+    hdf_id = loc_id
+
+    call h5ltmake_dataset_float_f(hdf_id, dataset_name, 1, dims, buffer, hdferr)
+
+  end subroutine
+        
+
+
+end module
