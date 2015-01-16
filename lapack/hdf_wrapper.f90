56ff496f2c7144324b3cb9e3d7f6a59a816e4c96
Peter Spackman <20265845@student.uwa.edu.au>
2015-01-16 16:17:15 +0800

    updated wrapper to now have methods for vec: real(4), real(8) and int(4)

--- lapack/hdf_wrapper.f90
+++ lapack/hdf_wrapper.f90
@@ -5,16 +5,18 @@ module hdf_wrapper
 
   implicit none
 
-  interface create_hdf_file
-    module procedure create_hdf_file
-  end interface
+  public
+
 
   interface create_dataset_vec
     module procedure cd_real_4_1d
+    module procedure cd_real_8_1d
+    module procedure cd_int_4_1d
   end interface
 
   interface create_dataset_mat
   end interface
+
 
 contains
 
@@ -22,6 +24,11 @@ contains
   subroutine init_library
     integer :: hdferr
     call H5open_f(hdferr)
+  end subroutine
+
+  subroutine close_library
+    integer :: hdferr
+    call H5close_f(hdferr)
   end subroutine
 
   subroutine create_hdf_file(filename, loc_id)
@@ -37,21 +44,49 @@ contains
   end subroutine
 
 
-  subroutine cd_real_4_1d(loc_id, dataset_name, rank, buffer)
-    integer, intent(in) :: loc_id, rank
+  subroutine cd_real_4_1d(loc_id, dataset_name, buffer)
+    integer, intent(in) :: loc_id
     character(len=*), intent(in) :: dataset_name
     real(4), dimension(:), intent(in) :: buffer
     integer :: hdferr
     integer(hid_t) :: hdf_id
     integer(hsize_t), dimension(1) :: dims
 
-    dims = (/ 1 /)
+    dims = shape(buffer)
     hdf_id = loc_id
 
     call h5ltmake_dataset_float_f(hdf_id, dataset_name, 1, dims, buffer, hdferr)
 
   end subroutine
-        
+  
+  subroutine cd_real_8_1d(loc_id, dataset_name, buffer)
+    integer, intent(in) :: loc_id
+    character(len=*), intent(in) :: dataset_name
+    real(8), dimension(:), intent(in) :: buffer
+    integer :: hdferr
+    integer(hid_t) :: hdf_id
+    integer(hsize_t), dimension(1) :: dims
 
+    dims = shape(buffer)
+    hdf_id = loc_id
 
+    call h5ltmake_dataset_double_f(hdf_id, dataset_name, 1, dims, buffer, hdferr)
+
+  end subroutine
+  
+  subroutine cd_int_4_1d(loc_id, dataset_name, buffer)
+    integer, intent(in) :: loc_id
+    character(len=*), intent(in) :: dataset_name
+    integer(4), dimension(:), intent(in) :: buffer
+    integer :: hdferr
+    integer(hid_t) :: hdf_id
+    integer(hsize_t), dimension(1) :: dims
+
+    dims = shape(buffer)
+    hdf_id = loc_id
+
+    call h5ltmake_dataset_int_f(hdf_id, dataset_name, 1, dims, buffer, hdferr)
+
+  end subroutine
+  
 end module
