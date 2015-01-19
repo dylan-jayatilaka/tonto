6a9398244eef7ecda7c6411bed90de496663c67c
Peter Spackman <20265845@student.uwa.edu.au>
2015-01-19 11:43:02 +0800

    implemented methods for integer(8), and 1D methods for complex(4) and complex(8) in h5output wrapper

--- lapack/h5output.f90
+++ lapack/h5output.f90
@@ -8,17 +8,20 @@ module h5output
 
   public
 
-
   interface create_dataset_vec
     module procedure cd_real_4_1d
     module procedure cd_real_8_1d
     module procedure cd_int_4_1d
+    module procedure cd_int_8_1d
+    module procedure cd_complex_4_1d
+    module procedure cd_complex_8_1d
   end interface
 
   interface create_dataset_mat
     module procedure cd_real_4_2d
     module procedure cd_real_8_2d
     module procedure cd_int_4_2d
+    module procedure cd_int_8_2d
   end interface
 
 
@@ -27,7 +30,9 @@ contains
   ! create file procedure
   subroutine init_hdf
     integer :: hdferr
+
     call H5open_f(hdferr)
+    
   end subroutine
 
 
@@ -84,7 +89,93 @@ contains
     call h5ltmake_dataset_double_f(hdf_id, dataset_name, 1, dims, buffer, hdferr)
 
   end subroutine
+
   
+  subroutine cd_complex_4_1d(loc_id, dataset_name, buffer)
+    integer, intent(in) :: loc_id
+    character(len=*), intent(in) :: dataset_name
+    complex(4), dimension(:), target, intent(in) :: buffer
+    integer :: hdferr
+    integer(hid_t) :: hdf_id, type_id, dspace_id, kind_type, dset_id
+    integer(hsize_t), dimension(1) :: dims
+    integer(hsize_t) :: offset
+    type(c_ptr) :: f_ptr
+
+    dims = shape(buffer)
+    hdf_id = loc_id
+
+    offset = c_sizeof(buffer(1))
+
+    ! Create a dataspace (aka r/w buffer)
+    call h5screate_simple_f(1, dims, dspace_id, hdferr)
+    
+    
+    ! create the memory datatype
+    call h5tcreate_f(H5T_COMPOUND_F, offset , type_id, hdferr)
+    offset = 0
+    call h5tinsert_f(type_id, "re", offset, H5T_NATIVE_REAL, hdferr)
+    offset = c_sizeof(buffer(1)) / 2
+    call h5tinsert_f(type_id, "im", offset, H5T_NATIVE_REAL, hdferr)
+    
+
+    ! create a dataset in the file to write to
+    call h5dcreate_f(hdf_id, dataset_name, type_id, dspace_id, dset_id, hdferr)
+
+    ! this is dangerous(assumes array indexed from 1,1
+    f_ptr = C_LOC(buffer(1))
+
+    ! write the buffer to file
+    CALL h5dwrite_f(dset_id, type_id, f_ptr, hdferr)
+
+    ! close both the dataset and the dataspace
+    CALL h5dclose_f(dset_id, hdferr)
+    CALL h5sclose_f(dspace_id, hdferr)
+
+  end subroutine
+
+  subroutine cd_complex_8_1d(loc_id, dataset_name, buffer)
+    integer, intent(in) :: loc_id
+    character(len=*), intent(in) :: dataset_name
+    complex(8), dimension(:), target, intent(in) :: buffer
+    integer :: hdferr
+    integer(hid_t) :: hdf_id, type_id, dspace_id, kind_type, dset_id
+    integer(hsize_t), dimension(1) :: dims
+    integer(hsize_t) :: offset
+    double precision :: b = 1.0
+    type(c_ptr) :: f_ptr
+
+    dims = shape(buffer)
+    hdf_id = loc_id
+
+    offset = c_sizeof(buffer(1))
+
+    ! Create a dataspace (aka r/w buffer)
+    call h5screate_simple_f(1, dims, dspace_id, hdferr)
+    
+    
+    ! create the memory datatype
+    call h5tcreate_f(H5T_COMPOUND_F, offset , type_id, hdferr)
+    offset = 0
+    call h5tinsert_f(type_id, "re", offset, H5T_NATIVE_DOUBLE, hdferr)
+    offset = c_sizeof(buffer(1)) / 2
+    call h5tinsert_f(type_id, "im", offset, H5T_NATIVE_DOUBLE, hdferr)
+    
+
+    ! create a dataset in the file to write to
+    call h5dcreate_f(hdf_id, dataset_name, type_id, dspace_id, dset_id, hdferr)
+
+    ! this is dangerous(assumes array indexed from 1,1
+    f_ptr = C_LOC(buffer(1))
+
+    ! write the buffer to file
+    CALL h5dwrite_f(dset_id, type_id, f_ptr, hdferr)
+
+    ! close both the dataset and the dataspace
+    CALL h5dclose_f(dset_id, hdferr)
+    CALL h5sclose_f(dspace_id, hdferr)
+
+  end subroutine
+
 
   subroutine cd_int_4_1d(loc_id, dataset_name, buffer)
     integer, intent(in) :: loc_id
@@ -102,6 +193,42 @@ contains
   end subroutine
 
 
+  subroutine cd_int_8_1d(loc_id, dataset_name, buffer)
+    integer, intent(in) :: loc_id
+    character(len=*), intent(in) :: dataset_name
+    integer(8), dimension(:), target, intent(in) :: buffer
+    integer :: hdferr
+    integer(hid_t) :: hdf_id, dspace_id, kind_type, dset_id
+    integer(hsize_t), dimension(1) :: dims
+    type(c_ptr) :: f_ptr
+
+    dims = shape(buffer)
+    hdf_id = loc_id
+
+    ! We need to create the kind type for writing into storage,
+    ! THIS NEEDS TO BE CHANGED TO NOT USE 8 AS A MAGIC NUMBER
+    kind_type = h5kind_to_type(8, H5_INTEGER_KIND)
+
+    ! Create a dataspace (aka r/w buffer)
+    call h5screate_simple_f(1, dims, dspace_id, hdferr)
+
+    ! create a dataset in the file to write to
+    call h5dcreate_f(hdf_id, dataset_name, kind_type, dspace_id, dset_id, hdferr)
+
+    ! this is dangerous(assumes array indexed from 1,1
+    f_ptr = C_LOC(buffer(1))
+
+    ! write the buffer to file
+    CALL h5dwrite_f(dset_id, kind_type, f_ptr, hdferr)
+
+    ! close both the dataset and the dataspace
+    CALL h5dclose_f(dset_id, hdferr)
+    CALL h5sclose_f(dspace_id, hdferr)
+
+  end subroutine
+
+  ! MATRIX METHODS
+
   subroutine cd_real_4_2d(loc_id, dataset_name, buffer)
     integer, intent(in) :: loc_id
     character(len=*), intent(in) :: dataset_name
@@ -116,7 +243,6 @@ contains
     call h5ltmake_dataset_float_f(hdf_id, dataset_name, 2, dims, buffer, hdferr)
 
   end subroutine
- 
 
   subroutine cd_real_8_2d(loc_id, dataset_name, buffer)
     integer, intent(in) :: loc_id
@@ -148,5 +274,43 @@ contains
     call h5ltmake_dataset_int_f(hdf_id, dataset_name, 2, dims, buffer, hdferr)
 
   end subroutine
- 
+
+  subroutine cd_int_8_2d(loc_id, dataset_name, buffer)
+    integer, intent(in) :: loc_id
+    character(len=*), intent(in) :: dataset_name
+    integer(8), dimension(:,:), target, intent(in) :: buffer
+    integer :: hdferr
+    integer(hid_t) :: hdf_id, dspace_id, kind_type, dset_id
+    integer(hsize_t), dimension(2) :: dims
+    type(c_ptr) :: f_ptr
+
+    dims = shape(buffer)
+    hdf_id = loc_id
+    kind_type = h5kind_to_type(8,H5_INTEGER_KIND)
+
+    call h5screate_simple_f(2, dims, dspace_id, hdferr) ! creates a read/write buffer
+
+    call h5dcreate_f(hdf_id, dataset_name, kind_type, dspace_id, dset_id, hdferr)
+    f_ptr = C_LOC(buffer(1,1))
+    CALL h5dwrite_f(dset_id, kind_type, f_ptr, hdferr)
+    CALL h5dclose_f(dset_id, hdferr)
+    CALL h5sclose_f(dspace_id, hdferr)
+
+  end subroutine
+  
+  ! END OF MATRIX METHODS
+
+  subroutine create_dataset_str(loc_id, dataset_name, buffer)
+    integer, intent(in) :: loc_id
+    character(len=*), intent(in) :: dataset_name
+    character(len=*), intent(in) :: buffer
+    integer :: hdferr
+    integer(hid_t) :: hdf_id
+
+    hdf_id = loc_id
+
+    call h5ltmake_dataset_string_f(hdf_id, dataset_name, buffer, hdferr)
+
+  end subroutine
+
 end module
--- lapack/test.f90
+++ lapack/test.f90
@@ -5,11 +5,13 @@ program test
   implicit none
 
   character(len=20), parameter :: filename = "test.h5"
-  character(len=10), parameter :: dataset = "powers"
-  character(len=10), parameter :: dataset2 = "two-powers"
   integer :: i, j, loc_id
   double precision, dimension(100) :: powers
   double precision, dimension(1000,1000) :: two_powers
+  integer, dimension(500) :: three_powers
+  integer(8), dimension(40,20) :: eight
+  integer(8), dimension(50) :: one_dimension
+  complex(8), dimension(30) :: c
 
   ! initialize our data. 
   call init_hdf
@@ -17,14 +19,29 @@ program test
   ! i => element in dataspace
 
   powers = (/ (2.0 ** i, i = 1,100) /)
+  three_powers = (/ (i ** 3, i = 1, 500) /)
+  eight = reshape((/ (i * 2, i = 1,40*20)/), shape(eight))
+  one_dimension = (/ (i + 5, i = 1, 50) /)
+  c = (/ (sqrt(cmplx(i,i-1)), i = 1,30) /)
 
   call create_hdf_file(filename, loc_id)
 
   call random_number( two_powers )
 
+  print *, "Finished setting data"
+  print *, c
+  call flush
+
   ! create a new hdf5 file using defaults, truncating file if it exists
-  call create_dataset_vec(loc_id, dataset, powers)
-  call create_dataset_mat(loc_id, dataset2, two_powers)
+  call create_dataset_vec(loc_id, "powers", powers)
+
+  print *, "powers done"
+  call flush
+  call create_dataset_vec(loc_id, "complex", c)
+  call create_dataset_vec(loc_id, "one-dimension", one_dimension)
+  call create_dataset_vec(loc_id, "three-powers", three_powers)
+  call create_dataset_mat(loc_id, "two-powers", two_powers)
+  call create_dataset_mat(loc_id, "eight", eight)
 
   call close_hdf
 
