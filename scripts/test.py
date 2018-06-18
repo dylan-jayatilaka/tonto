90212cc25b0fe09f35137c9a8e6b5ec936fc081a
Peter Spackman <peterspackman@fastmail.com>
2018-06-18 10:42:13 +0100

    Read in gaussian wavefunctions

--- scripts/test.py
+++ scripts/test.py
@@ -16,12 +16,13 @@ log = logging.getLogger('test')
 prefixes_to_ignore = ['Wall-clock', 'CPU time', 
     'Version', 'Platform', 'Timer', 'Build-date',
     'Warning', 'https', 'www', 'Peter', 'Daniel', 'Dylan',
-    'time taken for']
+    'Look', 'time taken for']
 
 test_categories = ['short', 'cx', 'long', 'geminal', 'relativistic']
 
 def is_junk(line):
-    return any(map(line.startswith, prefixes_to_ignore))
+    return (any(map(line.startswith, prefixes_to_ignore)) or 
+            line.strip == '')
 
 
 def isclose(a, b, abs_tol=0.0, rel_tol=0.0):
