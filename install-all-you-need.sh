444c2c3c0e27cad29e5c70b28c163180beee2bdc
Dylan Jayatilaka <dylan.jayatilaka@gmail.com>
2023-06-27 12:16:34 +0200

    Add the install-all-you-need.sh script

---
+++ install-all-you-need.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Run this script as "sudo ./install-all-you-need.sh"
+apt-get update
+apt-get upgrade
+apt-get install g++
+apt-get install gfortran-9
+apt-get install git
+apt-get install libblas-dev libblas3
+apt-get install liblapack-dev
+apt-get install cmake
+apt-get install coreutils
+apt-get install gawk
+apt-get install tofrodos
